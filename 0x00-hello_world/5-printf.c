38060d93b1a9662f753754acf5f26843e97e4824
Dave <ddave3568@gmail.com>
2023-09-07 20:16:42 -0700

    Now or never

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main- A program that prints a line using printf function
+ * main - Work please
  *
- * reutn: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 /**
  * main - A program that print computer size
+ *
  * Return 0 (Success)
+ * i
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("size of a char: %lu byte(s)\n",(unsigned long)size of (a));
-printf("size of a int: %lu byte(s)\n",(unsigned long)size of (b));
-printf("size of a long: %lu byte(s)\n",(unsigned long)size of (c));	
-printf("size of a long long int: %lu byte(s)\n",(unsigned long)size of (d));
-printf("size of a float: %lu byte(s)\n",(unsigned long)size of (fa));
+printf("size of a char: %d byte(s)\n",size of (char));
+printf("size of a int: %d byte(s)\n",size of (int));
+printf("size of a long int: %d byte(s)\n",size of (long int));	
+printf("size of a long long int: %d byte(s)\n",size of (long long int));
+printf("size of a float: %d byte(s)\n",size of (float));
          return (0);
 }
