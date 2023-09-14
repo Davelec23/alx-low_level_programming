f0c5104cff6a065469433f86c18ed0d0c530ba14
Dave <ddave3568@gmail.com>
2023-09-14 11:49:34 -0700

    Functions_Nested_Loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * A function thatshow the sign of no.-Entry
+ * Return :0
+ */
+int print_sign(int n)
+{
+	if(n>0){
+	_putchar('+');
+	return (1);
+	}	
+	else if(n<0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	}
+	return(0);
+}
