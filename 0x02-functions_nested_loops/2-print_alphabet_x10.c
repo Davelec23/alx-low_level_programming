d0006ece413b230132d73c62a33fee9770c44073
Dave <ddave3568@gmail.com>
2023-09-14 06:08:22 -0700

    The second and third Task at once

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet ten times in a new line - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+int a= '0';
+
+while (a<10)
+
+{
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+                _putchar(c);
+                c++;
+
+        }
+        _putchar('\n');
+      a++;
+}
+}
