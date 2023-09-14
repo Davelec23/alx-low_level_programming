aed03599801f850bb4183a2ecebd27902b923a93
Dave <ddave3568@gmail.com>
2023-09-14 12:47:47 -0700

    Function_Nested_Loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _abs-Prints the absolute value of the no.
+ *  * @a: the int to check
+ * Return: The absolute value of int
+ */
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+return (-a);
+}
